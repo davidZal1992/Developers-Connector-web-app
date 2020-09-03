ac4df375ae7cdef2a8bf098e1590fd8d4384f531
David <davidzal@post.bgu.ac.il>
2020-09-03 18:24:41 +0300

    remove fb login

--- client/src/components/auth/social-login/SocialLogin.js
+++ client/src/components/auth/social-login/SocialLogin.js
@@ -1,21 +1,16 @@
 import React,{Fragment} from 'react';
 import { GoogleLogin } from 'react-google-login';
-import FacebookLogin from 'react-facebook-login';
 import {GoogleLoginButton} from "react-social-login-buttons";
 import {connect} from 'react-redux';
-import {googleAuth,facebookAuth} from '../../../actions/auth'
+import {googleAuth} from '../../../actions/auth'
 import './SocialLogin.css'
 
-const SocialLogin = ({googleAuth,facebookAuth}) =>{ 
+const SocialLogin = ({googleAuth}) =>{ 
 
     const googleResponse =  (response) =>{
       googleAuth(response.tokenId);
     }  
 
-    const facebookResponse =  (response) =>{
-      facebookAuth(response.accessToken,response.userID)
-    // githubAuth(response.tokenId);
-  }   
   return (
     <Fragment>
       <div className="mt">
@@ -28,18 +23,10 @@ const SocialLogin = ({googleAuth,facebookAuth}) =>{
             )}
             cookiePolicy={'single_host_origin'}
          />
-         <FacebookLogin
-            appId="235443407778826"
-            fields="name,email,picture"
-            callback={facebookResponse}
-            cssClass="my-facebook-button-class"
-            icon="fa-facebook"
-            redirectUri
-        />,
         </div>
     </Fragment>
   )
 }
 
 
-export default connect(null,{googleAuth,facebookAuth})(SocialLogin);
+export default connect(null,{googleAuth})(SocialLogin);
